127bdecbb8a25236b2973f6b1315b6e1e369db4e
Kunj <shahkunj42@gmail.com>
2022-08-16 16:22:10 -0400

    commit tree bebfc7cea3408608e9bd86c0dc6fb0f649667135

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/signup', to: 'user#create'
+
+  get '/me', to: 'user#show'
+
+  post '/login', to: 'session#create'
+
+  delete "/logout", to: "session#destroy"
 end
